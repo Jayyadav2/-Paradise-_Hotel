1663fdc82e3c4ac778067241e0ea28c2ce5a2d73
Jay Yadav <jskk15536@gmail.com>
2024-09-11 19:43:34 +0530

    add the project

---
+++ src/main/java/com/hotel/Paradise_Hotel/ParadiseHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.Paradise_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadiseHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadiseHotelApplication.class, args);
+	}
+
+}
